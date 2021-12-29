c4e805589108fc1fbdf81ae57fffa88ef47d1d02
killercd <killercd@gmail.com>
2021-12-29 14:39:27 +0100

    First commit

---
+++ db_fast.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.Qt import QApplication, QClipboard
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPlainTextEdit
+from PyQt5.QtCore import QSize
+
+class ExampleWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(440, 240))    
+        self.setWindowTitle("DB Fast") 
+
+        # Add text field
+        self.b = QPlainTextEdit(self)
+        self.b.insertPlainText("You can write text here.\n")
+        self.b.move(10,10)
+        self.b.resize(400,200)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = ExampleWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
